40be061e45c3b5f6de201a96789d0594566befba
sharad singh <sharadksingh67@gmail.com>
2025-01-20 14:37:16 +0530

    v1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>About StocksApp</h1>
+      <h1>About StocksApp - V1</h1>
       <p>This is a simple application for Stocks.</p>
     </div>
   );
